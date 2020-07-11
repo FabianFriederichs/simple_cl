097b2d43d522f6ab2fc9df66fba054bfd9725ff9
Fabian Friederichs <fabian.friederichs@gmail.com>
2020-07-11 19:47:03 +0200

    .

--- cmake/include_cl_kernel.cmake
+++ cmake/include_cl_kernel.cmake
@@ -11,11 +11,9 @@ endfunction()
 ### Example for the template header. File should have ending *.hpp.in or *.h.in if we follow common practice.
 # #ifndef _CL_KERNEL_${kernel_name}_HPP_
 # #define _CL_KERNEL_${kernel_name}_HPP_
-#
 # namespace kernels
 # {
-# 	const char* ${kernel_name}_src = R"(${kernel_source})";
-#   const char* ${kernel_name}_copt = "${kernel_compiler_options}";
+# 	const char* ${kernel_name}_src = R"KERNELSOURCE(${kernel_source})KERNELSOURCE";
+# 	const char* ${kernel_name}_copt = R"COMPILEROPT(${kernel_compiler_options})COMPILEROPT";
 # }
-#
 # #endif
